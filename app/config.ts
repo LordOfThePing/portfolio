91efe8d5142d779326e14947f671f3d0fd320ceb
Pedro Andrés Flynn <pedro.flynn@middlesoft.com.uy>
2025-02-16 21:35:15 -0300

    add profile pic & metadata

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,17 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
-  ogImage: "/opengraph-image.png",
+  baseUrl: "https://flynnpedroa.netlify.app/",
+  title: "flynn.pa",
+  pageTitle: "Pedro Andres Flynn's portfolio",
+  name: "Pedro Andres Flynn",
+  ogImage: "/favicon.ico",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Pedro Andres Flynn is a software engineer who loves to build things for the web.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/pepeflynn",
+  github: "https://github.com/LordOfThePing",
+  instagram: "https://www.instagram.com/pedroflynn_/",
+  linkedin: "https://www.linkedin.com/in/pedro-flynn/",
+  email: "mailto:flynnpedroa@gmail.com",
 };
