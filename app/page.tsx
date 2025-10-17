74918c3885daee48173ba01eec64ae0e30ee03cf
LordOfThePing <pedro.flynn@middlesoft.com.uy>
2025-10-17 10:27:51 -0300

    particles

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,31 @@
 import ProjectsCollage from "./projectscollage/page";
 import Introduction from "./introduction/page";
 import ExperienceBanners from "./experiencebanners/page";
-
+import Particles from "./components/particles";
 
 export default function Page() {
   return (
-    <section>
-      <Introduction />
-      
-      <ExperienceBanners />
-      <ProjectsCollage />
+    <main className="relative min-h-screen overflow-hidden">
+      {/* Full-page background */}
+      <div className="fixed inset-0 -z-10">
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={400}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
 
-    </section>
+      {/* Page content */}
+      <section className="relative z-10">
+        <Introduction />
+        <ExperienceBanners />
+        <ProjectsCollage />
+      </section>
+    </main>
   );
 }
